3ef8a004d9dae29bd692247dc178fc76a08eea7a
Vlad <accierro@gmail.com>
2018-10-17 23:56:08 +0100

    App restructure. Add server base files. Adjust README file.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Traverse
+
+## How to run a local server?
+
+1.  Download [node.js](https://nodejs.org/en/download/) to your computer.
+2.  Open **Traverse** folder in _Command Line_ or _Terminal_.
+3.  Use `node index.js` command.
+4.  Open a browser and go to `http://localhost:3000`.
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+//Server set up
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", __dirname + "/views");
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+// Default home page
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+// Post route from home page
+app.post("/", (req, res) => {
+  res.send("Received POST request");
+});
+
+// Website listener, should be changed when website will be deployed
+app.listen(port, () =>
+  console.log("Traverse app listening on port " + port + "!")
+);

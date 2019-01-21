18add37f4b76fc58085ae93316186c63d0175af7
Vlad <accierro@gmail.com>
2019-01-21 16:34:20 +0000

    Add React Router functionality. Create SearchJourneyForm file. Set up the routes.

--- traverse-react/src/App.js
+++ traverse-react/src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import RestrictionCodeForm from './RestrictionCodeForm';
-import './App.css';
+import SearchJourneyForm from './SearchJourneyForm.js';
+import {Switch, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <RestrictionCodeForm />
+        <Switch>
+          <Route path='/restriction-codes' component={RestrictionCodeForm}/>
+          <Route path='/search-journey' component={SearchJourneyForm}/>
+        </Switch>
         <Footer />
       </div>
     );
--- traverse-react/src/Header.js
+++ traverse-react/src/Header.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Header extends Component{
   render(){
@@ -7,14 +8,14 @@ export default class Header extends Component{
     <div id="logo">
       <img src="images/traverselogo3.png" alt="Logo"/>
     </div>
-    
+
     <div id="nav-links">
       <ul>
         <li><a href="">Create Account</a></li>
         <li><a href="">Log In</a></li>
         <li><a href="">Help</a></li>
-        <li><a href="">Ticket Restriction Codes</a></li>
-        <li><a href="">Search Journey</a></li>
+        <li><Link to='/restriction-codes'>Ticket Restriction Codes</Link></li>
+        <li><Link to='/search-journey'>Search Journey</Link></li>
       </ul>
     </div>
   </div>

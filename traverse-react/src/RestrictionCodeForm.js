38475b724c24ceda746601d0454acd4620e29ae0
GarethBennett90 <g.bennett@lancaster.ac.uk>
2019-01-21 16:08:25 +0000

    Minor cosmetic changes, added instructions for the restriction form

--- traverse-react/src/RestrictionCodeForm.js
+++ traverse-react/src/RestrictionCodeForm.js
@@ -4,6 +4,25 @@ export default class RestrictionCodeForm extends Component{
   render(){
     return(
       <div id="main-body">
+      <div id="restriction-code-information">
+        <h2>Ticket Restriction Codes</h2>
+          <p>
+          Ticket restriction codes convey an important message about your ticket. 
+          These could dictate when, where, or on which service you can travel. 
+          They communicate these messages extremely poorly.
+          </p>
+           <p>
+           These codes are not consistently printed in the same placed on ticket, so finding them may be difficult. 
+           Don’t worry, because they do have one thing in common: they are always two characters long e.g. L1, G3, 56, HY.
+          </p>
+          <p>
+          They can be found on the front side of your ticket, typically in the bottom right corner. 
+          If you cannot find a two-character code, you may be eligible for any route.
+          </p>
+          <p>
+          To better understand your journey, enter your restriction code and hit Search.
+          </p>
+      </div>
         <div id="ticket">
           <div id="ticket-restriction-code-search">
             <h2>Search Ticket Restriction Code</h2>

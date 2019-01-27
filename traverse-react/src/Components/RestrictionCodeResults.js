dd201d7fe6ec815e218e771b9c7fc73ce36379cf
GarethBennett90 <garethbennett90@gmail.com>
2019-01-27 11:56:37 +0000

    added radio buttons for the days of the week

--- traverse-react/src/Components/RestrictionCodeResults.js
+++ traverse-react/src/Components/RestrictionCodeResults.js
@@ -32,7 +32,24 @@ export default class RestrictionCodeResults extends Component{
         <div id="main-body">
         <div id="results">
           <h2>Ticket Restriction Code</h2>
-          <h2 id="current-code">??</h2>
+          <h2 id="current-code">??</h2><br/><br/>
+
+          <div id="day-list">
+            <input type="radio" id="mon" name="day"/>
+            <label for="mon">Monday</label>
+            <input type="radio" id="tue" name="day"/>
+            <label for="tue">Tuesday</label>
+            <input type="radio" id="wed" name="day"/>
+            <label for="wed">Wednesday</label>
+            <input type="radio" id="thu" name="day"/>
+            <label for="thu">Thursday</label>
+            <input type="radio" id="fri" name="day"/>
+            <label for="fri">Friday</label>
+            <input type="radio" id="sat" name="day"/>
+            <label for="sat">Saturday</label>
+            <input type="radio" id="sun" name="day"/>
+            <label for="sun">Sunday</label>
+          </div>
 
           <h3>Outward Restrictions</h3>
           <img src="images/calendarIcon.png" align="center" height="30px" width="30px"></img><span>Day</span><br/>

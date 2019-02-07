e7fda1be328e57b3c31e329ae805d55aff3f4e04
Vladislavs Murzjonks <accierro@gmail.com>
2019-02-07 17:44:32 +0000

    Add a new dependency. Refactor SearchJourneyFormInputs component. Implement some basic functionality for inputting data.

--- traverse-react/src/components/SearchJourneyForm.js
+++ traverse-react/src/components/SearchJourneyForm.js
@@ -3,12 +3,78 @@ import Options from './SearchJourneyFormOptions';
 import Inputs from './SearchJourneyFormInputs'
 
 export default class SearchJourneyForm extends Component{
-  
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedDate: new Date(),
+      origin: "",
+      destination: ""
+    }
+
+    this.handleDateSelect = this.handleDateSelect.bind(this);
+    this.handleDateIncrement = this.handleDateIncrement.bind(this);
+    this.handleDateDecrement = this.handleDateDecrement.bind(this);
+    this.handleOriginInput = this.handleOriginInput.bind(this);
+    this.handleDestinationInput = this.handleDestinationInput.bind(this);
+    this.handleSwap = this.handleSwap.bind(this);
+  }
+
+  handleDateSelect(date){
+    this.setState({selectedDate: date})
+  }
+  handleDateIncrement(){
+    this.setState((prevState) =>{
+      return {
+        selectedDate: new Date(prevState.selectedDate.getTime() + 86400000)
+      }
+    })
+  }
+  handleDateDecrement(){
+    let {selectedDate} = this.state;
+    let today = parseInt(new Date().getTime() / 86400000) * 86400000;
+    let selectedDateMillis = selectedDate.getTime();
+    if(selectedDateMillis - 86399999 > today){
+      this.setState((prevState) =>{
+        return {
+          selectedDate: new Date(prevState.selectedDate.getTime() - 86400000)
+        }
+      })
+    }
+  }
+  handleOriginInput(e){
+    this.setState({origin: e.target.value})
+    console.log(this.state.origin);
+  }
+  handleDestinationInput(e){
+    this.setState({destination: e.target.value})
+  }
+  handleSwap(){
+    this.setState((prevState) =>{
+      return {
+        origin: prevState.destination,
+        destination: prevState.origin
+      }
+    })
+  }
+
   render(){
+    let {selectedDate, origin, destination} = this.state;
     return (
       <div id="main-body">
+        <h2> {selectedDate.getMonth() + " " + selectedDate.getDate()}</h2>
         <Options/>
-        <Inputs/>
+        <Inputs
+          incrementDate={this.handleDateIncrement}
+          decrementDate={this.handleDateDecrement}
+          onClickDate={this.handleDateSelect}
+          dateValue={this.state.selectedDate}
+          onOriginChange={this.handleOriginInput}
+          origin={origin}
+          onDestinationChange={this.handleDestinationInput}
+          destination={destination}
+          onSwap={this.handleSwap}
+          showCalendar={true}
+        />
       </div>
     );
   }
--- traverse-react/src/components/SearchJourneyFormInputs.js
+++ traverse-react/src/components/SearchJourneyFormInputs.js
@@ -1,28 +1,39 @@
 import React, {Component} from 'react';
+import Calendar from 'react-calendar';
 
+const SearchJourneyFormInputs = (props) => {
 
-export default class SearchJourneyFormInputs extends Component{
-  render(){
-    return (
-      <div id="search-journey-inputs">
-        <div id="one-leg">
-          <div id="search-journey-stations">
-            <input type="text" placeholder="Origin Station"/>
-            <img id="swapLoc" src="/images/swapArrows.png" alt=""/>
-            <input type="text" placeholder="Destination Station"/>
-          </div>
+  const inputDate = (date) => {
+    return `${date.getDate().toString().padStart(2,"0")}/${date.getMonth().toString().padStart(2, "0")}/${date.getFullYear()}`
 
-          <div id="search-journey-date">
-            <img src="/images/calendarIcon.png" alt="" height="25px" width="auto"/>
-            <input type="text" placeholder="Wed, 20th Feb"/>
-            <img className="date-increment" src="/images/leftArrow.png" height="20px" width="auto" alt=""/>
-            <img className="date-increment" src="/images/rightArrow.png" height="20px" width="auto" alt=""/>
-          </div>
+  }
+  return (
+    <div id="search-journey-inputs">
+      <div style={{visibility: props.showCalendar ? 'visible' : 'hidden' }}>
+        <Calendar
+          minDate={new Date()}
+          onClickDay={(value) => props.onClickDate(value)}
+        />
+      </div>
+      <div id="one-leg">
+        <div id="search-journey-stations">
+          <input onChange={props.onOriginChange} value={props.origin} type="text" placeholder="Origin Station"/>
+          <img onClick={props.onSwap} id="swapLoc" src="/images/swapArrows.png" alt=""/>
+          <input onChange={props.onDestinationChange} value={props.destination} type="text" placeholder="Destination Station"/>
         </div>
 
-        <input id="add-leg" value="Add Leg" type="submit"/>
-        <input id="search-journey-submit" value="Search" type="submit"/>
+        <div id="search-journey-date">
+          <img  src="/images/calendarIcon.png" alt="" height="25px" width="auto"/>
+          <input type="text" value={inputDate(props.dateValue)} disabled />
+          <img onClick={props.decrementDate} className="date-increment" src="/images/leftArrow.png" height="20px" width="auto" alt=""/>
+          <img onClick={props.incrementDate} className="date-increment" src="/images/rightArrow.png" height="20px" width="auto" alt=""/>
+        </div>
       </div>
-    )
-  }
+
+      <input id="add-leg" value="Add Leg" type="submit"/>
+      <input id="search-journey-submit" value="Search" type="submit"/>
+    </div>
+  )
 }
+
+export default SearchJourneyFormInputs;
